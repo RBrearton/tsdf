6bbdb44ac6842e3dc33fc9f3739e03d2733415dc
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-14 22:42:47 +0100

    Added some basic functionality

--- src/core/structs/uninitialized_tsdf_metadata.rs
+++ src/core/structs/uninitialized_tsdf_metadata.rs
@@ -13,3 +13,29 @@ pub(super) struct UninitializedTsdfMetadata<'a> {
     /// The IoMode used to open the file.
     io_mode: IoMode,
 }
+
+impl<'a> UninitializedTsdfMetadata<'a> {
+    /// Constructs a new UninitializedTsdfMetadata.
+    pub fn new(current_version: &'a str, file_format: Option<FileFormat>, io_mode: IoMode) -> Self {
+        Self {
+            current_version,
+            file_format,
+            io_mode,
+        }
+    }
+
+    /// Returns the current version of the tsdf crate.
+    pub fn get_current_version(&self) -> &str {
+        self.current_version
+    }
+
+    /// Returns the file format of the file.
+    pub fn get_file_format(&self) -> &Option<FileFormat> {
+        &self.file_format
+    }
+
+    /// Returns the IoMode used to open the file.
+    pub fn get_io_mode(&self) -> &IoMode {
+        &self.io_mode
+    }
+}
