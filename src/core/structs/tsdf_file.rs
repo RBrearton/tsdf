4e102b65d03ccc4d5cc32d74740564bc30a77128
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-14 18:12:46 +0100

    Implementd TsdfFileTrait

--- src/core/structs/tsdf_file.rs
+++ src/core/structs/tsdf_file.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, path::Path};
 
-use crate::core::enums::FileFormat;
+use crate::core::enums::{FileFormat, IoMode};
 use crate::core::traits::TsdfFileTrait;
 
 use super::Dir;
@@ -15,6 +15,50 @@ pub struct TsdfFile<'a, 'b> {
     /// The mode used to write the file.
     file_format: FileFormat,
 
+    /// The IoMode used to open the file.
+    io_mode: IoMode,
+
     /// The open file handle.
     file: File,
+}
+
+impl TsdfFileTrait for TsdfFile<'_, '_> {
+    fn get_version(&self) -> &str {
+        self.version
+    }
+
+    fn get_path(&self) -> &Path {
+        self.path
+    }
+
+    fn get_io_mode(&self) -> &IoMode {
+        &self.io_mode
+    }
+
+    fn get_file_format(&self) -> &FileFormat {
+        &self.file_format
+    }
+
+    fn get_size(&self) -> u64 {
+        self.file.metadata().unwrap().len()
+    }
+
+    fn get_root_dir(&self) -> &Dir {
+        unimplemented!()
+    }
+
+    fn new(path: &'static Path, io_mode: IoMode, file_format: FileFormat) -> Self {
+        let file = File::open(path).unwrap();
+
+        // Get the version from cargo.
+        let version = env!("CARGO_PKG_VERSION");
+
+        TsdfFile {
+            path,
+            version,
+            file,
+            file_format,
+            io_mode,
+        }
+    }
 }
