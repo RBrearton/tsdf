e376521ce11485b6646a20e33a79f8c19de90998
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-14 18:12:27 +0100

    Played around with the TsdfFileTrait

--- src/core/traits/tsdf_file_trait.rs
+++ src/core/traits/tsdf_file_trait.rs
@@ -1,17 +1,20 @@
 use std::path::Path;
 
-use crate::core::enums::FileFormat;
+use crate::core::enums::{FileFormat, IoMode};
 use crate::core::structs::Dir;
 
-pub(crate) trait TsdfFileTrait {
+pub trait TsdfFileTrait {
     /// Returns the version of the file.
     fn get_version(&self) -> &str;
 
     /// Returns the path to the file.
     fn get_path(&self) -> &Path;
 
-    /// Returns the write mode of the file.
-    fn get_write_mode(&self) -> FileFormat;
+    /// Returns the io mode of the file.
+    fn get_io_mode(&self) -> &IoMode;
+
+    /// Returns the file format of the file.
+    fn get_file_format(&self) -> &FileFormat;
 
     /// Returns the size of the file, in bytes.
     fn get_size(&self) -> u64;
@@ -19,11 +22,6 @@ pub(crate) trait TsdfFileTrait {
     /// Returns the root Dir of the file.
     fn get_root_dir(&self) -> &Dir;
 
-    /// Constructs a new TsdfFileTrait reader from the path provided as an argument.
-    fn new_reader(path: &Path) -> Self;
-
-    /// Constructs a new TsdfFileTrait writer from the path provided as an argument. Please note
-    /// that, if the file already exists, the WriteMode must match the mode used to write the file,
-    /// if specified.
-    fn new_writer(path: &Path, write_mode: FileFormat) -> Self;
+    /// Constructs a new TsdfFileTrait, taking a path, IoMode and FileFormat as arguments.
+    fn new(path: &'static Path, io_mode: IoMode, file_format: FileFormat) -> Self;
 }
