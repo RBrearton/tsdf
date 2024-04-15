2e72646453e0ca5aecc92737862925c42051d324
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-15 22:43:21 +0100

    Made FileFormat equatable

--- src/core/enums/file_format.rs
+++ src/core/enums/file_format.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Enum to specify the file format of a file. Please note that, for all production use cases, the
 /// file format should be set to `Binary`. The `Text` mode is only for debugging and development.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum FileFormat {
     /// Write the file in binary mode. This is the recommended mode for all production use cases.
     Binary,
