ade0386bcaac000ab57e1f7cdd96a12f5bfa8389
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-15 21:34:27 +0100

    Updated IoMode enum to use new ReadMode and WriteMode enums

--- src/core/enums/io_mode.rs
+++ src/core/enums/io_mode.rs
@@ -1,27 +1,14 @@
 use serde::{Deserialize, Serialize};
 
+use super::{ReadMode, WriteMode};
+
 /// Enum for the different IO modes.
 #[derive(Debug, Serialize, Deserialize)]
 pub enum IoMode {
-    /// Read the file. This never places a lock on the file.
-    Read,
+    /// Read the file. This may or may not lock the file, depending on which ReadMode is being used.
+    Read(ReadMode),
 
-    /// Write to the file without locking it. Using this mode removes some functionality, such as
-    /// data deletion. Otherwise, data can be written to the file as normal.
-    LocklessWrite,
-
-    /// Write to the file with a lock. This mode allows for all functionality, including data
-    /// deletion, at the cost of not allowing readers to concurrently access the file.
-    LockingWrite,
-}
-
-// Now add a function to the IoMode enum that returns true if the IoMode is a write mode.
-impl IoMode {
-    pub fn is_a_writing_mode(&self) -> bool {
-        match self {
-            IoMode::Read => false,
-            IoMode::LocklessWrite => true,
-            IoMode::LockingWrite => true,
-        }
-    }
+    /// Write to the file. This may be a lockless writer, or a locking writer, depending on which
+    /// WriteMode is being used.
+    Write(WriteMode),
 }
