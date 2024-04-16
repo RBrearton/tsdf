183b383d247567e9fb56d034785403e883c02945
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-16 22:28:09 +0100

    Refactor FileSerializable trait to accept a file and IoMetadata in write and from_addr methods

--- src/core/traits/file_serializable.rs
+++ src/core/traits/file_serializable.rs
@@ -1,9 +1,11 @@
-use crate::core::structs::Addr;
+use std::fs::File;
+
+use crate::core::structs::{Addr, IoMetadata};
 
 pub(crate) trait FileSerializable {
     /// Writes the object to the file at the given location.
-    fn write(&self, addr: Addr);
+    fn write(&self, addr: Addr, file: &mut File, io_metadata: &IoMetadata);
 
     /// Reads the object from the file at the given location.
-    fn from_addr(addr: Addr) -> Self;
+    fn from_addr(addr: Addr, file: &mut File, io_metadata: &IoMetadata) -> Self;
 }
