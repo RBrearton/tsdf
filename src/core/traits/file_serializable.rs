95e8df588ee1cad3dd4b3b24be0c1d3f66b21a19
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-16 22:28:19 +0100

    Refactor FileSerializable trait to accept a file and IoMetadata in write method

--- src/core/traits/file_serializable.rs
+++ src/core/traits/file_serializable.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 
 use crate::core::structs::{Addr, IoMetadata};
 
+/// The FileSerializable trait is used to define objects that can be written to and read from files.
 pub(crate) trait FileSerializable {
     /// Writes the object to the file at the given location.
     fn write(&self, addr: Addr, file: &mut File, io_metadata: &IoMetadata);
