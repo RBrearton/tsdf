1dff448788e994e523df7b1c352f0cbe92aca2fb
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-16 23:20:18 +0100

    Refactor FileSerializable trait to include to_bytes and from_bytes methods

--- src/core/traits/file_serializable.rs
+++ src/core/traits/file_serializable.rs
@@ -4,6 +4,12 @@ use crate::core::structs::{Addr, IoMetadata};
 
 /// The FileSerializable trait is used to define objects that can be written to and read from files.
 pub(crate) trait FileSerializable {
+    /// Converts the object to a byte array.
+    fn to_bytes(&self) -> Vec<u8>;
+
+    /// Constructs the object from a byte array.
+    fn from_bytes(bytes: Vec<u8>) -> Self;
+
     /// Writes the object to the file at the given location.
     fn write(&self, addr: Addr, file: &mut File, io_metadata: &IoMetadata);
 
