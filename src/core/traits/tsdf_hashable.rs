15db6ca31d6d92593edaa36e3e3d1c7008f0f59c
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-18 16:36:49 +0100

    Implement TsdfHashable trait for types that implement the std library Hash trait

--- src/core/traits/tsdf_hashable.rs
+++ src/core/traits/tsdf_hashable.rs
@@ -4,3 +4,10 @@ use crate::core::structs::tsdf_hash::TsdfHash;
 pub(crate) trait TsdfHashable {
     fn hash(&self) -> TsdfHash;
 }
+
+// Implement this for all types that implement the std library Hash trait.
+impl<T: std::hash::Hash> TsdfHashable for T {
+    fn hash(&self) -> TsdfHash {
+        TsdfHash::new(&self)
+    }
+}
