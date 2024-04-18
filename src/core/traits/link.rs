ff438249353312b686d1a57e0ee1cbd644f3a408
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-18 16:11:56 +0100

    Update Link trait to include new methods and imports

--- src/core/traits/link.rs
+++ src/core/traits/link.rs
@@ -1,8 +1,19 @@
-use crate::core::enums::LinkPtr;
+use std::fs::File;
+
+use crate::core::{enums::LinkPtr, structs::IoMetadata};
 
 use super::Locatable;
 
 pub(crate) trait Link: Locatable {
+    /// Returns a reference to the next link.
     fn get_next(&self) -> &LinkPtr;
+
+    /// Returns the link number of this link.
     fn get_link_number(&self) -> i32;
+
+    /// Returns a reference to the file that the shard is stored in.
+    fn get_file(&self) -> &File;
+
+    /// Returns the metadata needed to read/write to the file.
+    fn get_io_metadata(&self) -> &IoMetadata;
 }
