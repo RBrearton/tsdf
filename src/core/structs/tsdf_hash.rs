d0aceccf97fe74817d76a767d2966a62c6e8dbf0
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-18 16:13:46 +0100

    Refactor TsdfHash struct to use DefaultHasher for hashing

--- src/core/structs/tsdf_hash.rs
+++ src/core/structs/tsdf_hash.rs
@@ -1,8 +1,26 @@
+use std::hash::DefaultHasher;
+
 use crate::core::traits::FileSerializable;
 
 /// The TsdfHash struct is a simple struct that holds a hash value. This hash
+
+#[derive(PartialEq, Eq)]
 pub(crate) struct TsdfHash {
     hash_value: u64,
+}
+
+impl TsdfHash {
+    /// Creates a new TsdfHash from a hashable value.
+    pub(crate) fn new<T: std::hash::Hash>(hashable: &T) -> Self {
+        // Create a new hasher.
+        let mut hasher = DefaultHasher::new();
+
+        // Hash the value.
+        hashable.hash(&mut hasher);
+        let hash_value = hasher.finish();
+
+        Self { hash_value }
+    }
 }
 
 impl FileSerializable for TsdfHash {
@@ -12,12 +30,12 @@ impl FileSerializable for TsdfHash {
     }
 
     fn to_json(&self) -> String {
-        serde_json::to_string(&self).unwrap()
+        serde_json::to_string(self).unwrap()
     }
 
     fn from_bytes(bytes: &[u8]) -> Self {
         let hash_value = u64::from_le_bytes(bytes[0..8].try_into().unwrap());
-        Self::new(hash_value)
+        Self { hash_value }
     }
 
     fn from_json(json: String) -> Self {
@@ -26,5 +44,28 @@ impl FileSerializable for TsdfHash {
 
     fn get_bin_size_on_disk() -> u64 {
         std::mem::size_of::<u64>() as u64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test that we can convert a TsdfHash to bytes and back.
+    #[test]
+    fn test_tsdf_hash_to_bytes() {
+        let hash = TsdfHash::new(&123);
+        let bytes = hash.to_bin();
+        let hash2 = TsdfHash::from_bytes(&bytes);
+        assert_eq!(hash, hash2);
+    }
+
+    /// Test that we can convert a TsdfHash to json and back.
+    #[test]
+    fn test_tsdf_hash_to_json() {
+        let hash = TsdfHash::new(&123);
+        let json = hash.to_json();
+        let hash2 = TsdfHash::from_json(json);
+        assert_eq!(hash, hash2);
     }
 }
