3988f035fdbbd89a5d241718b9e40d4419ba276b
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-18 17:46:35 +0100

    Refactor FileSerializable trait to include get_json_size_on_disk method

--- src/core/traits/file_serializable.rs
+++ src/core/traits/file_serializable.rs
@@ -7,12 +7,31 @@ use crate::core::{
 
 /// The FileSerializable trait is used to define objects that can be written to
 /// and read from files.
-pub(crate) trait FileSerializable {
-    /// Converts the object to a binary byte array.
+pub(crate) trait FileSerializable
+where
+    Self: serde::Serialize + serde::de::DeserializeOwned,
+{
+    /// Converts the object to a binary byte array. This should be hand
+    /// optimized for performance.
     fn to_bin(&self) -> Vec<u8>;
 
-    /// Converts the object to a json string.
-    fn to_json(&self) -> String;
+    /// Converts the object to a json string. We provide a default
+    /// implementation as this isn't performance critical and is only present
+    /// for debugging purposes.
+    fn to_json(&self) -> String {
+        let mut json_string = serde_json::to_string(self).unwrap();
+
+        // Pad the json string with spaces so that it is the same length as the
+        // largest possible json string.
+        let max_json_size = Self::get_json_size_on_disk();
+        let json_size = json_string.len() as u64;
+        let padding_size = max_json_size - json_size;
+        for _ in 0..padding_size {
+            json_string.push(' ');
+        }
+
+        json_string
+    }
 
     /// Constructs the object from a byte array.
     fn from_bin(bytes: &[u8]) -> Self;
@@ -25,7 +44,7 @@ pub(crate) trait FileSerializable {
     fn get_size_on_disk(&self, io_metadata: &IoMetadata) -> u64 {
         match io_metadata.get_tsdf_metadata().get_file_format() {
             FileFormat::Binary => Self::get_bin_size_on_disk(),
-            FileFormat::Text => self.get_json_size_on_disk(),
+            FileFormat::Text => Self::get_json_size_on_disk(),
         }
     }
 
@@ -33,12 +52,14 @@ pub(crate) trait FileSerializable {
     fn get_bin_size_on_disk() -> u64;
 
     /// Returns the size of the object once serialized to json, in bytes.
-    fn get_json_size_on_disk(&self) -> u64 {
-        // We aren't performance critical when using json, so we can just
-        // convert the object to json and get the length of the resulting
-        // string.
-        self.to_json().as_bytes().len() as u64
-    }
+    /// # BE CAREFUL
+    /// Please make sure that you consider the maximum possible size of the json
+    /// string when implementing this method. If you aren't sure, bigger is
+    /// better.
+    ///
+    /// Don't worry about performance in implementations. A good example of how
+    /// to implement this can be found in the TsdfHash struct.
+    fn get_json_size_on_disk() -> u64;
 
     /// Writes the object to the file at the given location.
     fn write(&self, addr: Addr, file: &File, io_metadata: &IoMetadata) {
