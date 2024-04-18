a3d90d665cd5a50d4d9c6221e5890473c1883ec3
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-18 16:33:35 +0100

    Significant improvements to DistDictShardTrait

--- src/core/traits/dist_dict_shard_trait.rs
+++ src/core/traits/dist_dict_shard_trait.rs
@@ -1,27 +1,25 @@
-use std::{fs::File, hash::Hash};
+use crate::core::structs::{Addr, TsdfHash};
 
-use crate::core::structs::{Addr, IoMetadata};
-
-use super::{FileSerializable, Link};
+use super::{FileSerializable, Link, TsdfHashable};
 
 /// The DistDictShard is part of a distributed dictionary. A DistDict is made up
 /// of multiple DistDictShards, each of which is responsible for a subset of the
 /// keys.
-pub(crate) trait DistDictShardTrait<TKey: Hash, TVal: FileSerializable>:
+pub(crate) trait DistDictShardTrait<TKey: TsdfHashable, TVal: FileSerializable>:
     Link
 {
     /// Returns whether the shard contains the given hash.
-    fn contains(&self, hash: u64) -> bool;
+    fn contains(&self, hash: TsdfHash) -> bool;
 
     /// Adds a key-value pair to the shard. Note that we take the hash of the
     /// key as an argument to avoid recomputing it.
     /// You must first make sure that the shard doesn't already contain the
     /// key's hash, or this function will overwrite the existing value.
-    fn add(&self, key: &TKey, val: &TVal, hash: u64);
+    fn add(&self, key: &TKey, val: &TVal, hash: TsdfHash);
 
     /// Removes a key-value pair from the shard. Note that we take the hash of
     /// the key as an argument to avoid recomputing it.
-    fn remove(&self, key: &TKey, hash: u64);
+    fn remove(&self, key: &TKey, hash: TsdfHash);
 
     /// Gets the location of the nth hash in the shard.
     fn get_hash_loc(&self, n: usize) -> Addr;
@@ -30,23 +28,25 @@ pub(crate) trait DistDictShardTrait<TKey: Hash, TVal: FileSerializable>:
     fn get_val_loc(&self, n: usize) -> Addr;
 
     /// Gets the hash of the nth key in the shard.
-    fn get_hash(&self, n: usize, file: &File, io_metadata: &IoMetadata) -> u64 {
-        let addr = self.get_hash_loc(n);
-
-        // The hash is stored as a u64, so we can read it directly from the
-        // file.
-        let mut bytes = vec![0; 8];
-        file.read_at(&mut bytes, addr.get_loc()).unwrap();
-
-        // Convert the bytes to a u64.
-        u64::from_le_bytes(bytes.try_into().unwrap())
+    fn get_hash(&self, n: usize) -> TsdfHash {
+        // Since TsdfHash is guaranteed to be FileSerializable, we can use the
+        // from_addr method to read the hash from the file.
+        TsdfHash::from_addr(
+            self.get_hash_loc(n),
+            self.get_file(),
+            self.get_io_metadata(),
+        )
     }
 
     /// Gets the value of the nth hash in the shard.
-    fn get_val(&self, n: usize, file: &File, io_metadata: &IoMetadata) -> TVal {
+    fn get_val(&self, n: usize) -> TVal {
         // Since TVal is guaranteed to be FileSerializable, we can use the
         // from_addr method to read the value from the file.
-        TVal::from_addr(self.get_val_loc(n), &file, io_metadata)
+        TVal::from_addr(
+            self.get_val_loc(n),
+            self.get_file(),
+            self.get_io_metadata(),
+        )
     }
 
     /// Returns the number of keys in the shard.
