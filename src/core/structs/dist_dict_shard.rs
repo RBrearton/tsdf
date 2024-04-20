ec473a6763b120e4a0e2dff8795ada0b982cac83
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-20 13:36:58 +0100

    Refactor TsdfHash struct to include PhantomData

--- src/core/structs/dist_dict_shard.rs
+++ src/core/structs/dist_dict_shard.rs
@@ -1,13 +1,13 @@
 use std::fs::File;
+use std::marker::PhantomData;
 
 use crate::core::traits::Locatable;
-use crate::core::traits::TsdfHashable;
 use crate::core::{
     enums::LinkPtr,
     traits::{DistDictShardTrait, FileSerializable, Link},
 };
 
-use super::{Addr, IoMetadata, TsdfHash};
+use super::{Addr, IoMetadata};
 
 /// The DistDictShard struct is a shard of a distributed dictionary. It is a
 /// collection of keys and values that lives on disk, which is why the
@@ -28,7 +28,23 @@ use super::{Addr, IoMetadata, TsdfHash};
 /// values. The hashes and values are interleaved, so that the first hash is
 /// followed by the first value, the second hash is followed by the second
 /// value, and so on.
-pub(crate) struct DistDictShard<'a, 'b> {
+pub(crate) struct DistDictShard<'a, 'b, TVal>
+where
+    TVal: FileSerializable,
+{
+    /// A throwaway variable to store the type of the value. We need this to
+    /// make sure that every instance of DistDictShard is only capable of
+    /// storing one kind of value. If we don't make this binding, then one
+    /// instance of DistDictShard would, in theory, be able to store any kind of
+    /// value, which would be a disaster.
+    ///
+    /// Specifically, the syntax for using functions defined in the
+    /// DistDictShardTrait would be ambiguous, as the compiler wouldn't know
+    /// which implementation of DistDictShardTrait to use. This way, when we
+    /// make a DistDictShard we constrain its value type, and the compiler can
+    /// work out which implementation of the generic DistDictShardTrait to use.
+    val: PhantomData<TVal>,
+
     /// The LinkPtr to the next shard.
     next: LinkPtr,
 
@@ -45,19 +61,28 @@ pub(crate) struct DistDictShard<'a, 'b> {
     file: &'b File,
 }
 
-impl DistDictShard<'_, '_> {
+impl<TVal> DistDictShard<'_, '_, TVal>
+where
+    TVal: FileSerializable,
+{
     //
 }
 
 // Implement the locatable trait for DistDictShard.
-impl Locatable for DistDictShard<'_, '_> {
+impl<TVal> Locatable for DistDictShard<'_, '_, TVal>
+where
+    TVal: FileSerializable,
+{
     fn get_loc(&self) -> &Addr {
         &self.loc
     }
 }
 
 // Implement the Link trait for DistDictShard.
-impl Link for DistDictShard<'_, '_> {
+impl<TVal> Link for DistDictShard<'_, '_, TVal>
+where
+    TVal: FileSerializable,
+{
     fn get_next(&self) -> &LinkPtr {
         &self.next
     }
@@ -76,117 +101,67 @@ impl Link for DistDictShard<'_, '_> {
 }
 
 // Implement the DistDictShardTrait for DistDictShard.
-impl<TKey, TVal> DistDictShardTrait<TKey, TVal> for DistDictShard<'_, '_>
+impl<TVal> DistDictShardTrait<TVal> for DistDictShard<'_, '_, TVal>
 where
-    TKey: TsdfHashable,
     TVal: FileSerializable,
 {
-    fn get_hash_loc(&self, n: usize) -> Addr {
-        // The location of the nth hash is the location of the shard plus the
-        // size of the next LinkPtr, plus the size of each hash and value up to
-        // the nth hash.
-        let size_of_next = LinkPtr::get_size_on_disk(self.io_metadata);
-
-        // The size of each hash is the size of a TsdfHash.
-        let size_of_hash = TsdfHash::get_size_on_disk(self.io_metadata);
-
-        // The size of each value is the size of a T.
-        let size_of_val = TVal::get_size_on_disk(self.io_metadata);
-
-        // The location of the nth hash is the location of the shard plus the
-        // size of the next LinkPtr, plus the size of each hash and value up to
-        // the nth hash.
-        let loc = self.loc.get_loc()
-            + size_of_next
-            + (size_of_hash + size_of_val) * n as u64;
-
-        Addr::new(loc)
-    }
-
-    fn get_val_loc(&self, n: usize) -> Addr {
-        // The location of the nth value is the location of the nth hash plus
-        // the size of the hash.
-        let size_of_hash = TsdfHash::get_size_on_disk(self.io_metadata);
-
-        let loc = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_hash_loc(self, n).get_loc() + size_of_hash;
-
-        Addr::new(loc)
-    }
-
     fn get_num_keys(&self) -> usize {
         // The number of keys in the shard should be equal to 8 times the link
         // number raised to the power of 2.
         (8 * self.link_number.pow(2)) as usize
     }
+}
 
-    fn contains(&self, hashed_key: &TsdfHash) -> bool {
-        // To check if the shard contains a hash, we need to calculate the hash
-        // modulo the number of keys to work out the hash's position in the
-        // shard.
-        let num_keys = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_num_keys(&self);
-        let n = hashed_key.get_hash_value() % num_keys as u64;
+#[cfg(test)]
+mod tests {
+    use tempfile::tempfile;
 
-        // Now we need to check if the hash at position n is equal to the hash
-        // we're looking for.
-        let hash_n = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_hash(&self, n as usize);
+    use crate::core::{
+        enums::{IoMode, WriteMode},
+        structs::TsdfMetadata,
+    };
 
-        *hashed_key == hash_n
-    }
+    use super::super::TsdfHash;
+    use super::*;
 
-    fn add(&self, hashed_key: &TsdfHash, val: TVal) {
-        // Get the location of the hash in the shard.
-        let num_keys = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_num_keys(&self);
-        let hash_table_idx = hashed_key.get_hash_table_idx(num_keys as u64);
+    /// Test that we can add a key-value pair to the shard and then remove it.
+    /// This test uses a text file format.
+    #[test]
+    fn test_add_remove() {
+        // Define the io metadata.
+        let io_metadata = IoMetadata::new(
+            TsdfMetadata::new(
+                "no_version".to_string(),
+                crate::core::enums::FileFormat::Text,
+            ),
+            IoMode::Write(WriteMode::LocklessWrite),
+        );
+        let file = tempfile().unwrap();
 
-        // Get the location of the hash and value in the shard.
-        let hash_loc = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_hash_loc(&self, hash_table_idx as usize);
+        let shard: DistDictShard<'_, '_, Addr> = DistDictShard {
+            next: LinkPtr::Null(Addr::new(0)),
+            link_number: 1,
+            loc: Addr::new(0),
+            io_metadata: &io_metadata,
+            file: &file,
+            val: std::marker::PhantomData,
+        };
 
-        let val_loc = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_val_loc(&self, hash_table_idx as usize);
+        // Create a key-value pair.
+        let key = "test_key".to_string();
+        let hashed_key = TsdfHash::new(&key);
+        let val = Addr::new(123);
 
-        // Write the hash and value to the file.
-        hashed_key.write(hash_loc, self.file, self.io_metadata);
-        val.write(val_loc, self.file, self.io_metadata);
-    }
+        // Add the key-value pair to the shard.
+        shard.add(&hashed_key, val);
 
-    fn remove(&self, hashed_key: &TsdfHash) {
-        // Get the location of the hash in the shard.
-        let num_keys = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_num_keys(&self);
-        let hash_table_idx = hashed_key.get_hash_table_idx(num_keys as u64);
+        // Check that the shard contains the key.
+        assert!(shard.contains(&hashed_key));
 
-        // Get the location of the hash and value in the shard.
-        let hash_loc = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_hash_loc(&self, hash_table_idx as usize);
+        // Remove the key-value pair from the shard.
+        shard.remove(&hashed_key);
 
-        let val_loc = <DistDictShard<'_, '_> as DistDictShardTrait<
-            TKey,
-            TVal,
-        >>::get_val_loc(&self, hash_table_idx as usize);
-
-        // Write the hash and value to the file.
-        TsdfHash::remove(hash_loc, self.file, self.io_metadata);
-        TVal::remove(val_loc, self.file, self.io_metadata);
+        // Check that the shard no longer contains the key.
+        assert!(!shard.contains(&hashed_key));
     }
 }
