091045483ff260035f26c660dc2eb4973a3d7050
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-24 22:03:04 +0100

    Wrote first pass at add method

--- src/core/traits/dist_dict_trait.rs
+++ src/core/traits/dist_dict_trait.rs
@@ -1,15 +1,101 @@
-use super::FileSerializable;
+use crate::core::{
+    enums::LinkPtr,
+    structs::{addr::Addr, DistDictShard},
+};
+
+use super::{
+    DistDictShardReader, DistDictShardWriter, FileSerializable, Link,
+    Locatable, TsdfHashable,
+};
 
 /// A distributed dictionary is a key-value store that is distributed across
 /// multiple shards. Anything that implements this pub(crate) trait can behave
 /// as a distributed dictionary.
-pub(crate) trait DistDictTrait<TKey: FileSerializable, TVal: FileSerializable> {
+pub(crate) trait DistDictTrait<TKey: TsdfHashable, TVal: FileSerializable>:
+    Locatable
+{
+    /// Returns the first shard in the distributed dictionary.
+    fn get_first_shard(&self) -> DistDictShard<'_, '_, TVal>;
+
     /// Adds a key-value pair to the dictionary.
-    fn add(&self, key: TKey, val: TVal);
+    fn add(&self, key: &TKey, val: &TVal) {
+        // Start by hashing the key.
+        let hashed_key = key.hash();
+
+        // Now that we've got the key, we need to find the first shard in the
+        // distributed dictionary that has space for the key.
+        let mut shard = self.get_first_shard();
+
+        loop {
+            // Work out which index the key should be stored at in the shard.
+            let num_keys = shard.get_num_keys();
+            let hash_table_idx = hashed_key.get_hash_table_idx(num_keys as u64);
+
+            // Check to see if there's already a value at that index.
+            let is_written = shard.is_hash_written(hash_table_idx as usize);
+
+            // If there's no value at that index, we can write the key-value
+            // pair to the shard.
+            if !is_written {
+                shard.add(&hashed_key, val);
+                return;
+            }
+
+            // If there is a value at that index, we need to check if the key
+            // is already in the shard.
+            let hash = shard.get_hash(hash_table_idx as usize);
+            if hashed_key == hash {
+                // If the key is already in the shard, we can update the value
+                // associated with the key.
+                shard.add(&hashed_key, val);
+                return;
+            }
+
+            // If the key isn't in the shard, we need to move to the next shard.
+            let next = shard.get_next();
+            let link_number = shard.get_link_number();
+
+            match next {
+                // If the next pointer is null, we need to create a new shard.
+                LinkPtr::Null(_) => {
+                    // Create a new shard at the end of the file.
+                    let file_end = self.get_file().metadata().unwrap().len();
+                    let new_shard_addr = Addr::new(file_end);
+                    let new_shard = DistDictShard::<TVal>::new(
+                        link_number + 1,
+                        new_shard_addr,
+                        self.get_io_metadata(),
+                        self.get_file(),
+                        false,
+                    );
+
+                    // Set the next pointer of the current shard to point to the
+                    // new shard.
+                    shard.set_next(&LinkPtr::Addr(new_shard_addr));
+
+                    // Set the new shard as the current shard.
+                    shard = new_shard;
+                }
+
+                // If the next pointer is an address, we need to load the next
+                // shard, which is stored at that address.
+                LinkPtr::Addr(addr) => {
+                    // Load the next shard.
+                    shard = DistDictShard::new(
+                        link_number + 1,
+                        Addr::new(addr.get_loc()),
+                        self.get_io_metadata(),
+                        self.get_file(),
+                        true,
+                    )
+                }
+            }
+        }
+    }
 
     /// Removes a key-value pair from the dictionary.
-    fn remove(&self, key: TKey);
+    fn remove(&self, key: &TKey);
 
     /// Gets the value associated with the given key.
-    fn get(&self, key: TKey) -> TVal;
+    fn get(&self, key: &TKey) -> TVal;
 }
