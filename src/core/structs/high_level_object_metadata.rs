41eb1ea2ecaf44416a5163e9366b45bea56c42c7
Richard Brearton <richard.brearton@racingbulls.com>
2024-04-28 11:15:31 +0100

    Commented out unused, non-working struct

--- src/core/structs/high_level_object_metadata.rs
+++ src/core/structs/high_level_object_metadata.rs
@@ -1,6 +1,6 @@
 use super::DistDict;
 
-pub(super) struct HighLevelObjectMetadata {
-    name: String,
-    metadata: DistDict<String>,
-}
+// pub(super) struct HighLevelObjectMetadata {
+//     name: String,
+//     metadata: DistDict<String, String>,
+// }
