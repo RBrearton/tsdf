263ab0abae046945d9c09e788812f669dfafc9d6
Richard Brearton <richard.brearton@racingbulls.com>
2024-05-02 22:13:38 +0100

    Refactor DistListShardReader to use ShardTrait instead of Link supertrait

--- src/core/traits/dist_list_shard_reader.rs
+++ src/core/traits/dist_list_shard_reader.rs
@@ -1,9 +1,9 @@
-use super::{FileSerializable, Link, VariableSizeOnDisk};
+use super::{FileSerializable, ShardTrait, VariableSizeOnDisk};
 
 /// TThis trait defines everything we need to be able to do to read from a
 /// shard of a distributed list in a file.
 pub(crate) trait DistListShardReader<T: FileSerializable>:
-    Link + VariableSizeOnDisk
+    VariableSizeOnDisk + ShardTrait<T>
 {
     /// Returns the number of elements in the shard.
     fn get_num_elements(&self) -> usize;
