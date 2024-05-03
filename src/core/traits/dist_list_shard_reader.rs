11e3feec694d93ab464b0e2f89f0aea36fa67c32
Richard Brearton <richard.brearton@racingbulls.com>
2024-05-03 23:09:45 +0100

    Refactor DistListShardReader to include additional methods for element retrieval and address retrieval

--- src/core/traits/dist_list_shard_reader.rs
+++ src/core/traits/dist_list_shard_reader.rs
@@ -1,3 +1,5 @@
+use crate::core::structs::Addr;
+
 use super::{FileSerializable, ShardTrait, VariableSizeOnDisk};
 
 /// TThis trait defines everything we need to be able to do to read from a
@@ -5,6 +7,22 @@ use super::{FileSerializable, ShardTrait, VariableSizeOnDisk};
 pub(crate) trait DistListShardReader<T: FileSerializable>:
     VariableSizeOnDisk + ShardTrait<T>
 {
-    /// Returns the number of elements in the shard.
-    fn get_num_elements(&self) -> usize;
+    /// Returns the nth element in the shard.
+    fn get_element(&self, n: usize) -> T;
+
+    /// Returns the nth is_element_written boolean in the shard.
+    fn is_element_written(&self, n: usize) -> bool;
+
+    /// Returns the address of the nth element in the shard.
+    fn get_element_addr(&self, n: usize) -> Addr;
+
+    /// Returns the address of the nth is_element_written boolean in the shard.
+    fn get_is_element_written_addr(&self, n: usize) -> Addr;
+
+    /// Returns whether the shard is full.
+    fn is_full(&self) -> bool {
+        // To find out if the shard is full, we check the final
+        // is_element_written boolean in the shard.
+        self.is_element_written(self.get_capacity() - 1)
+    }
 }
